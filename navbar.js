dcaef82d1c472184af8f26105018cf9faebb5a03
Leonardo Amorim <74081681+Leow14@users.noreply.github.com>
2022-03-11 14:35:10 -0300

    Add files via upload

---
+++ navbar.js
@@ -0,0 +1,43 @@
+class MobileNavbar {
+    constructor(mobileMenu, navlist, navLink) {
+        this.mobileMenu = document.querySelector(mobileMenu);
+        this.navList = document.querySelector(navList);
+        this.navLinks = document.querySelectorAll(navLinks);
+        this.activeClass = "active";
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+
+    animateLinks() {
+        this.navLinks.forEach((link, index) => {
+            console.log(index / 7 + 0.3);
+            link.style.animation
+            ? (link.style.animation = "")
+            : (link.style.animation = 'navLinkFade 0.5s ease forwards 0.3s');
+        });
+    }
+    handleClick() {
+        this.navList.classList.toggle(this.activeClass);
+        this.mobileMenu.classList.toggle(this.activeClass); 
+        this.animateLinks();
+    }
+
+    addClickEvent() {
+        this.mobileMenu.addEventListener("click", this.handleClick);
+    }
+init() {
+if(this.mobileMenu) {
+    this.addClickevent();
+}
+return this; 
+    }
+
+}
+
+const MobileNavbar = new MobileNavbar(
+    ".mobile-menu",
+    ".nav-list",
+    ".nav-list li",
+);
+MobileNavbar.init();
